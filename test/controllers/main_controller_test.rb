3fff8944ff8753b4163ca6fddb1c2e2fc5ace354
Daniil Matveev <d.matveev@voiso.com>
2025-04-23 16:13:45 +0400

    fix: add tests

---
+++ test/controllers/main_controller_test.rb
@@ -0,0 +1,19 @@
+class MainControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get root_url
+    assert_response :success
+    assert_not_nil assigns(:posts)
+    assert_template :index
+  end
+
+  test "should list all posts" do
+    post1 = posts(:one)
+    post2 = posts(:two)
+
+    get root_url
+    assert_response :success
+
+    assert_match post1.title, @response.body
+    assert_match post2.title, @response.body
+  end
+end
