e54846773504faa3bb309ce9b80eb787b1a3cedc
Daniil Matveev <d.matveev@voiso.com>
2025-04-29 16:13:09 +0400

    fix: update linter

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,21 +49,21 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comments
-      @comments = Post.find(params[:id]).comments
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_comments
+    @comments = Post.find(params[:id]).comments
+  end
 
-    def set_post
-      @post = Post.find(params[:id])
-    end
+  def set_post
+    @post = Post.find(params[:id])
+  end
 
-    def set_categories
-      @categories = Category.all
-    end
+  def set_categories
+    @categories = Category.all
+  end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :body, :category_id)
-    end
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:title, :body, :category_id)
+  end
 end
