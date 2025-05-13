0872c8155aa15e9309f38426268726832510a6f1
Daniil Matveev <d.matveev@voiso.com>
2025-05-13 20:24:24 +0400

    fix: db schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_250_513_153_921) do
+ActiveRecord::Schema[7.1].define(version: 20_250_512_185_400) do
   create_table 'categories', force: :cascade do |t|
     t.string 'name', null: false
     t.datetime 'created_at', null: false
@@ -37,11 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 20_250_513_153_921) do
     t.integer 'post_id', null: false
     t.datetime 'created_at'
     t.datetime 'updated_at'
-    t.integer 'user'
-    t.integer 'post'
-    t.index ['post'], name: 'index_post_likes_on_post'
     t.index ['post_id'], name: 'index_post_likes_on_post_id'
-    t.index ['user'], name: 'index_post_likes_on_user'
     t.index %w[user_id post_id], name: 'index_post_likes_on_user_id_and_post_id', unique: true
     t.index ['user_id'], name: 'index_post_likes_on_user_id'
   end
